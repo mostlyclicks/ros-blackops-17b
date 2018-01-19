05762791c34254565581b04fea7156b101dac9ef
mostlyClicks <carlos@mostlyclicks.com>
2018-01-19 16:51:13 -0700

    filter to action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_filter :set_cache_headers
+  before_action :set_cache_headers
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
